5e84c229fccd3d1b594eacfa3d36e5fe36ff54ef
Ylgr <quyen.luminous@gmail.com>
2019-07-11 01:19:39 +0700

    add test and migration

---
+++ test/adoption.js
@@ -0,0 +1,36 @@
+var Adoption = artifacts.require("Adoption");
+
+contract("Adoption", function(accounts) {
+  it("should assert true", function(done) {
+    var adoption = Adoption.deployed();
+    assert.isTrue(true);
+    done();
+  });
+  describe('First group of tests', () => {
+    let instance;
+
+    before(async () => {
+      instance = await Adoption.deployed();
+    });
+
+    it('User should adopt a pet', async () => {
+      await instance.adopt.sendTransaction(8, {from: accounts[0]});
+      let adopter = await instance.adopters.call(8);
+      assert.equal(adopter, accounts[0],"Incorrect owner address");
+    });
+
+    it('Should get adopter address by pet id in array', async () => {
+      let adopters = await instance.getAdopters.call();
+      assert.equal(adopters[8], accounts[0], "Owner of pet id should be recorder in the array");
+    })
+
+    it('Should throw if invalid pet id is given', async () => {
+      try {
+        await instance.adopt.sendTransaction(17,{from: accounts[0]});
+        assert.fail(true, failed, "This function did not throw");
+      } catch (error) {
+        assert.include(String(error), "revert", `Expected "revert" but instead got ${error}`);
+      }
+    })
+  });
+});
